0cf63dc497ab5a9f610128b96c23afef01de6664
mrugendrashilvant <sanketshilvant@gmail.com>
2021-08-20 23:34:00 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Input from './components/Input';
+import React from 'react';
+import Buttons from './components/Buttons';
+import Details from './components/Details';
 
 function App() {
+  const [text, setText] = React.useState("");
+  const [words, setWords] = React.useState(0);
+  const [char, setChar] = React.useState(0);
+  const[mode, setMode] = React.useState("light");
+
+  const toggleMode = ()=>{
+    mode === "light" ? setMode("dark") : setMode("light");
+
+    mode === "light" ? document.body.style.backgroundColor="#121212" : document.body.style.backgroundColor="white"
+  }
+
+  let changeText = (e) => {
+    let input = e.target.value;
+    let countWords = input.split(" ");
+    let countChar = input.split("");
+    setText(input);
+    let length = countWords.length;
+    if(countWords.length === 1){
+      if(countWords[0] === ""){
+        length = 0;
+      }
+    }
+    
+    setWords(length);
+    setChar(countChar.length);
+    //console.log(text)
+  }
+
+  const toLowerCase = () => {
+    setText(text.toLowerCase());
+    //console.log("editing to lower case")
+  }
+
+  const toUpperCase = () => {
+    setText(text.toUpperCase());
+    //console.log("editing to uppercase")
+  }
+
+  const clearText = ()=>{
+    setText("");
+    setWords(0);
+    setChar(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Text Analyzer" setMode={toggleMode} mode={mode}/>
+      <Input changeText={changeText} text={text} mode={mode} />
+      <Buttons tolc={toLowerCase} touc={toUpperCase} clearText={clearText} mode={mode}/>
+      <Details words={words} chars={char} mode={mode}/>
     </div>
   );
 }
